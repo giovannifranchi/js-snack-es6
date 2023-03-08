de0bb0d0c824129a07f27d25bcbc5ce4e5ae7f50
giovannifranchi <giovanni.franchi95@gmail.com>
2023-03-08 17:13:05 +0100

    Fix: backticks in string array

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,4 +1,4 @@
-const arrayInvitati = [ 'Brad Pitt’, ‘Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+const arrayInvitati = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
 
 const invitedPeopleObjArr = arrayInvitati.map((invited, index)=>{
     return {tableName: 'Tavolo Vip', name: invited, tablePlace: index}
