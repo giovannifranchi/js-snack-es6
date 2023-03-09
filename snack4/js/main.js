b37a01c798e33d60fcabc437613c8d27e4fdfb5a
giovannifranchi <giovanni.franchi95@gmail.com>
2023-03-09 22:46:36 +0100

    Fix: assign random num to object list

--- snack4/js/main.js
+++ snack4/js/main.js
@@ -1,17 +1,19 @@
+'use scrict';
+
 function createRndNum(min ,max){
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 class Teams {
-    constructor(name, maxPoints, maxFouls){
+    constructor(name){
         this.name = name;
-        this.points = createRndNum(0, maxPoints);
-        this.fouls = createRndNum(0, maxFouls);
+        this.points = 0;
+        this.fouls = 0;
     }
 }
 
-function createArrayOfTeams(teamList, maxPoints, maxFouls){
-    return teamList.map((element)=> new Teams(element, maxPoints, maxFouls));
+function createArrayOfTeams(teamList){
+    return teamList.map((element)=> new Teams(element));
 }
 
 const maxPoints = 90;
@@ -19,22 +21,44 @@ const maxFouls = 50;
 const teamList = ['Juventus', 'Milan', 'Inter', 'Sampdoria', 'Torino', 'Chievo', 'Napoli'];
 
 
-const teamObjList = createArrayOfTeams(teamList, maxPoints, maxFouls);
+const teamObjList = createArrayOfTeams(teamList);
 
-const teamFinalList = teamObjList.map((element)=>{
+const teamObjRandom = teamObjList.map((element)=> {
+    return {
+        name: element.name,
+        points: createRndNum(0, maxPoints),
+        fouls: createRndNum(0, maxPoints),
+    }
+})
+
+
+const teamFinalList = teamObjRandom.map((element)=>{
     const {name} = element;
-    let {fouls} = element;
+    const {fouls} = element;
     return {name, fouls};
 });
 
 // Other concise solution
 
-const finalTeams = teamList.map((element)=> new Teams(element, maxPoints, maxFouls))
+const finalTeams = teamList.map((element)=> new Teams(element))
+.map((element)=> {
+    return {
+        name: element.name,
+        points: createRndNum(0, maxPoints),
+        fouls: createRndNum(0, maxPoints),
+    }
+})
 .map((element)=>{
     const {name} = element;
     const {fouls} = element;
     return {name, fouls};
-})
+});
 
+// console.log(teamObjList, 'obj list');
+
+console.log(teamObjList);
+console.log(teamObjRandom);
+console.log(teamFinalList);
 console.log(finalTeams);
+
 
